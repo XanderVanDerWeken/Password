d13a970df1adda685463497d4a64cecc28d463cd
xa3687va <xander.van.der.weken@fh-erfurt.de>
2025-03-09 19:55:25 +0100

    Added config object

---
+++ internal/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"log"
+
+	"github.com/greencoda/confiq"
+	confiqjson "github.com/greencoda/confiq/loaders/json"
+)
+
+type ConfigStruct struct {
+	serverPort int `cfg:"serverPort"`
+}
+
+func LoadConfig() ConfigStruct {
+	configSet := confiq.New()
+
+	if err := configSet.Load(
+		confiqjson.Load().FromFile("./config.json"),
+	); err != nil {
+		log.Fatal(err)
+	}
+
+	var cfg ConfigStruct
+
+	if err := configSet.Decode(&cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	return cfg
+}
